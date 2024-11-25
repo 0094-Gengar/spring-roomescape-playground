2eb8035549e66c627834b95afb321641da0b521c
sinji <tobeabestns@naver.com>
2024-11-26 02:03:32 +0900

    feat(TimeRequest): [8단계] 예약 시간 요청에 사용할 DTO 생성

---
+++ src/main/java/roomescape/model/time/TimeRequest.java
@@ -0,0 +1,19 @@
+package roomescape.model.time;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class TimeRequest {
+    @JsonProperty("time")
+    private String time;
+
+    public TimeRequest() {
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
